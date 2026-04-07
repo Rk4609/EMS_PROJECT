3e7ab76df59b15fd6095c175eff4f5f640276a57
Rk4609 <rakeshjangid8742@gmail.com>
2026-04-07 16:39:48 +0530

    first commit

---
+++ Readme.md
@@ -0,0 +1,1 @@
+# EMS projects
---
+++ models/auth.model.js
@@ -0,0 +1,13 @@
+import mongoose, { Schema } from "mongoose"
+
+const employeeSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, unique: true, required: true, lowercase: true },
+    password: { type: String, required: true },
+    role: { type: String, default: "employee" }
+  },
+  { timestamps: true },
+)
+
+export const Employee = mongoose.model("Employee", employeeSchema)
